bf904416374f328831fefc74633424f1596f1973
Murat Çakmak <murat@seyum.com>
2021-05-01 16:31:58 +0300

    Initial commit

---
+++ geriSayim/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace geriSayim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbSaat = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbDakika = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.cmbSaniye = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbGun = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbsecim = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbSaat
+            // 
+            this.cmbSaat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSaat.FormattingEnabled = true;
+            this.cmbSaat.Location = new System.Drawing.Point(97, 42);
+            this.cmbSaat.Name = "cmbSaat";
+            this.cmbSaat.Size = new System.Drawing.Size(121, 21);
+            this.cmbSaat.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Saat";
+            // 
+            // cmbDakika
+            // 
+            this.cmbDakika.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDakika.FormattingEnabled = true;
+            this.cmbDakika.Location = new System.Drawing.Point(97, 73);
+            this.cmbDakika.Name = "cmbDakika";
+            this.cmbDakika.Size = new System.Drawing.Size(121, 21);
+            this.cmbDakika.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dakika";
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(12, 203);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(73, 13);
+            this.lbldurum.TabIndex = 5;
+            this.lbldurum.Text = "Kalan Zaman:";
+            // 
+            // cmbSaniye
+            // 
+            this.cmbSaniye.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSaniye.FormattingEnabled = true;
+            this.cmbSaniye.Location = new System.Drawing.Point(97, 100);
+            this.cmbSaniye.Name = "cmbSaniye";
+            this.cmbSaniye.Size = new System.Drawing.Size(121, 21);
+            this.cmbSaniye.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Saniye";
+            // 
+            // cmbGun
+            // 
+            this.cmbGun.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGun.FormattingEnabled = true;
+            this.cmbGun.Location = new System.Drawing.Point(97, 12);
+            this.cmbGun.Name = "cmbGun";
+            this.cmbGun.Size = new System.Drawing.Size(121, 21);
+            this.cmbGun.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gün";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Yapılacak";
+            // 
+            // cmbsecim
+            // 
+            this.cmbsecim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbsecim.FormattingEnabled = true;
+            this.cmbsecim.Items.AddRange(new object[] {
+            "Bilgisayarı Kapat",
+            "Yeniden Başlat"});
+            this.cmbsecim.Location = new System.Drawing.Point(97, 127);
+            this.cmbsecim.Name = "cmbsecim";
+            this.cmbsecim.Size = new System.Drawing.Size(121, 21);
+            this.cmbsecim.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 227);
+            this.Controls.Add(this.cmbsecim);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbGun);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbSaniye);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbDakika);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSaat);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Geri Sayım";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbSaat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbDakika;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbldurum;
+        private System.Windows.Forms.ComboBox cmbSaniye;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbGun;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbsecim;
+    }
+}
+
---
+++ geriSayim/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace geriSayim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false; // thread da iken ui erişimi için.
+        }
+        Thread saatGuncelleme;
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            for (int i = 0; i <= 24; i++)
+            {
+                cmbSaat.Items.Add(i);
+            }
+
+            for (int i = 0; i <= 60; i++)
+            {
+                cmbDakika.Items.Add(i);
+                cmbSaniye.Items.Add(i);
+                cmbGun.Items.Add(i);
+            }
+
+            cmbGun.SelectedIndex = 0;
+            cmbSaat.SelectedIndex = 0;
+            cmbDakika.SelectedIndex = 0;
+            cmbSaniye.SelectedIndex = 0;
+            cmbsecim.SelectedIndex = 0;
+
+        }
+        
+        int gun = 0;
+        int saat = 0;
+        int dakika = 0;
+        int saniye = 0;
+        bool acikKapali = false;
+        private void button1_Click(object sender, EventArgs e)
+        {          
+            
+            if ((int)cmbGun.Items[cmbGun.SelectedIndex] == 0 && (int)cmbSaat.Items[cmbSaat.SelectedIndex] == 0 && (int)cmbDakika.Items[cmbDakika.SelectedIndex] == 0 && (int)cmbSaniye.Items[cmbSaniye.SelectedIndex] == 0)
+            {
+                MessageBox.Show("Zaman Belirtin.");
+            }
+            else
+            {
+                if(!acikKapali)
+                {                    
+                    gun = (int)cmbGun.Items[cmbGun.SelectedIndex];
+                    saat = (int)cmbSaat.Items[cmbSaat.SelectedIndex];
+                    dakika = (int)cmbDakika.Items[cmbDakika.SelectedIndex];
+                    saniye = (int)cmbSaniye.Items[cmbSaniye.SelectedIndex];
+                    saatGuncelleme = new Thread(saatGuncelle);
+                    saatGuncelleme.Start();
+                    acikKapali = true;
+                }
+                else
+                {
+                    if(MessageBox.Show("Zaten Geri Sayım Açık Kapatılsın Mı ?","Geri Sayım", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            saatGuncelleme.Abort();
+                            acikKapali = false; // thread kapatıldı.
+                        }
+                        catch (ThreadAbortException ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                    }
+                   
+                }
+                
+            }
+        }
+        
+        void saatGuncelle()
+        {
+            while (true)
+            {
+                //saniye sıfır ise saniyeyi den daha çıkarma yapmamış olup eksilere gitmesini önlemiş oluruz.
+                if (saniye != 0)
+                {
+                    Thread.Sleep(1000);
+                    saniye -= 1;
+                }
+
+                if (saniye == 0 && dakika == 0 && saat == 0 && gun == 0) // saniye dakika saat, gun sıfırlandığında geri sayım bitmiş demektir.
+                {
+                    lbldurum.Text = "Kalan Zaman: Saat:" + saat + " Dakika:" + dakika + " Saniye:" + saniye + " Tamamlandı.";
+                    Thread.Sleep(2000);// Kullanıcının tamamlandı yazısını görebilmesi için 2 saniye daha bekletiyorum.
+                    if(cmbsecim.SelectedIndex == 0)
+                        Shutdown("1"); // Bilgisayarı Kapat.                          
+                    else
+                        Shutdown("2"); // Yeniden Başlat.
+                    break; // İş parçacığını(Thread) sonlandırmak için. döngüden break ile çıkış yapıyoruz.
+                    
+                }
+                else
+                    lbldurum.Text = "Kalan Zaman: Gün:"+gun+" Saat:" + saat + " Dakika:" + dakika + " Saniye:" + saniye + "";
+
+                if (saniye <= 0) // saniye 0 eşit veya her ihtimale karşı 0 dan küçük ise.
+                {
+
+                    if (dakika != 0) // dakika var mı kontrol et varsa saniye 60 ver dakikadan 1 azalt. Bu sayede zamanı sadece 1 saniyeler ile bitirebiliyoruz.
+                    {
+                        dakika -= 1;
+                        saniye = 60;
+                    }
+                    else // dakika yoksa saati kontrol et.
+                    {
+                        if (saat != 0) // saat var mı varsa dakikaya 60 ver saatten bir azalt.
+                        {
+                            saat -= 1;
+                            dakika = 60;
+                        }
+                        else
+                        {
+                            if (gun != 0)
+                            {
+                                gun -= 1;
+                                saat = 24;
+                            }
+                            else
+                            {
+                                //kontrol bitişi. // yıl gibi devam edebilir :)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+      
+
+        public void Shutdown(string flag) // Bilgisayarı Kapatma, Yeniden Başlatma Flag 1 kapatır 2 Yeniden Başlatır.
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                     mcWin32.GetMethodParameters("Win32Shutdown");
+
+            mboShutdownParams["Flags"] = flag;
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown",
+                                               mboShutdownParams, null);
+            }
+        }
+    }
+}
